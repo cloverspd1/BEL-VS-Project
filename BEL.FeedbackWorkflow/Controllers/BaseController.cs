2caac3701eeebddf35cd42ffe0d53a791fa70d29
cloverspd1 <cloverspd1@bajajelectricals.com>
2018-12-21 15:20:15 +0530

    Final Change In LUM Requirement

--- BEL.FeedbackWorkflow/Controllers/BaseController.cs
+++ BEL.FeedbackWorkflow/Controllers/BaseController.cs
@@ -115,7 +115,7 @@
                         this.SPCurrentUser = new UserDetails();
                         //this.SPCurrentUser.UserId = "9";//Sharepoint Admin
                         // this.SPCurrentUser.UserId = "20";//dcrdcnuser1
-                        this.SPCurrentUser.UserId = "27";//collabapp2
+                        //this.SPCurrentUser.UserId = "27";//collabapp2
                         //this.SPCurrentUser.UserId = "22";//collabapp3
                         // this.SPCurrentUser.UserId = "41";//Dumbre Amit
                         // this.SPCurrentUser.UserId = "43";//Dnyadev 
@@ -125,7 +125,7 @@
                         //this.SPCurrentUser.UserId = "39";//Gadhari suresh
                         //this.SPCurrentUser.UserId = "113";//virendra
                         //this.SPCurrentUser.UserId = "30";//bhatnagar
-                        //this.SPCurrentUser.UserId = "35";//Udole Sachin
+                        this.SPCurrentUser.UserId = "35";//Udole Sachin
                         //this.SPCurrentUser.UserId = "9";//Admin
                         //this.SPCurrentUser.UserId = "34";//Creator sent back by Quality //tapas
                         //this.SPCurrentUser.UserId = "32";//sanin
